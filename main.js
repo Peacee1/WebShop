eeb5db5fa43c06c76e31c56b5f3c08762a6b68ad
Peacee1 <103803050+Peacee1@users.noreply.github.com>
2023-12-20 15:58:56 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  //click cho picsbutton and memebutton
+  const productPicsElement = document.getElementById("product-pics");
+  productPicsElement.addEventListener("click", () => {
+    console.log(productPicsElement);
+    const picsButton = document.getElementById("nonedisplaybox1");
+    const memeButton = document.getElementById("nonedisplaybox2");
+    if (picsButton.style.display !== "flex") {
+      picsButton.style.display = "flex";
+    } else {
+      picsButton.style.display = "none";
+    }
+    memeButton.style.display = "none";
+  });
+
+  const productMemeElement = document.getElementById("product-meme");
+  productMemeElement.addEventListener("click", () => {
+    const picsButton = document.getElementById("nonedisplaybox1");
+    const memeButton = document.getElementById("nonedisplaybox2");
+    if (memeButton.style.display !== "flex") {
+      memeButton.style.display = "flex";
+    } else {
+      memeButton.style.display = "none";
+    }
+    picsButton.style.display = "none";
+  });
+  //tạo sự kiện click cho giỏ hàng
+  const cart = document.getElementById("cart");
+  cart.addEventListener("click", function () {
+    const cartbox = document.getElementById("cartbox");
+    if (cartbox.style.transform !== "translateY(0%)") {
+      cartbox.style.transform = "translateY(0%)";
+    } else {
+      cartbox.style.transform = "translateY(100%)";
+    }
+  });
+  //Làm giỏ hàng
+});
 
